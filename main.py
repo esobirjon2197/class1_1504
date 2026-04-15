0ebfc4bab18edaf50c9dbfc359300d0c7c715ca6
Sobirjon Eshniyazov <esobirjon2197@gmail.com>
2026-04-15 14:57:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,310 @@
+#  1-m
+class Mashina:
+    gildirak = 4
+
+m1 = Mashina()
+print(f"m1 gildirak: {m1.gildirak}")
+
+m2 = Mashina()
+m2.rang = "Yashil"
+print(f"m2 rang: {m2.gildirak}")
+
+
+m3 = Mashina()
+m3.madel = "Damas"
+print(f"m3 madel: {m3.madel}")
+
+
+# 2-m
+class Telefon:
+    brand = "Samsung"
+
+
+t1 = Telefon()
+print(f"t1.brand: {t1.brand}")
+
+t2 = Telefon()
+t2.narx = 1234
+print(f"t2.narx: {t2.narx}")
+
+t3 = Telefon()
+t3.xotira = 128
+print(f"t3.xotira: {t3.xotira}")
+
+
+# 3-m
+class Kitob:
+    sahifa = 200
+
+k1 = Kitob()
+print(f"Kitob sahifa: {k1.sahifa}")
+
+k2 = Kitob()
+k2.nomi = "Harry"
+print(f"Kitob nomi: {k2.nomi}")
+
+
+k3 = Kitob()
+k3.muallif = "Mualif yo'q"
+print(f"Kitob mualif: {k3.muallif}")
+
+
+
+# 4-m
+class Talaba:
+    kurs = 1
+
+t1 = Talaba()
+print(f"kurs: {t1.kurs}")
+
+t2= Talaba()
+t2.ism = "Bunyod"
+print(f"ism: {t2.ism}")
+
+t3 = Talaba()
+t3.yosh = 15
+print(f"yosh: {t3.yosh}")
+
+
+
+# 5-m
+class Kompyuter:
+    ram = 8
+
+k1 = Kompyuter()
+print(f"ram : {k1.ram}")
+
+k2 = Kompyuter()
+k2.protsesor = 18
+print(f"protsesor : {k2.protsesor}")
+
+k3 = Kompyuter()
+k3.ssd = 2
+print(f"ssd : {k3.ssd}")
+
+
+# 6-m
+class Mashina:
+    rang = "OQ"
+    tezlik = 140
+    narx = 654321
+
+t1 = Mashina()
+print(f"t1 malumotlar: ")
+print(f"obyekyning rang: {t1.rang}")
+print(f"obyekyning tezlik: {t1.tezlik}")
+
+print(f"obyekyning narx: {t1.narx}")
+
+# 7-m
+class Telefon:
+    narx = 12345
+    kamera = 140
+    batareya = 100
+
+t1 = Telefon()
+print(f"t1 malumotlar: ")
+print(f"obyekyning narx: {t1.narx}")
+print(f"obyekyning kamera: {t1.kamera}")
+print(f"obyekyning batareya: {t1.batareya}")
+
+
+# 8-m
+class Kitob:
+    nomi = '......'
+    yili = 2000
+    janr  = 200
+
+t1 = Kitob()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nomi: {t1.nomi}")
+print(f"obyekyning yili: {t1.yili}")
+print(f"obyekyning janr : {t1.janr }")
+
+
+# 9-m
+class Talaba:
+    ism = "Bunyod"
+    fakulted = "Alxorazimiy-vorislari"
+    baho = 2
+
+t1 = Talaba()
+print(f"t1 malumotlar: ")
+print(f"obyekyning ism: {t1.ism}")
+print(f"obyekyning fakulted: {t1.fakulted}")
+print(f"obyekyning baho : {t1.baho }")
+
+
+# 10-m
+class Kompyuter:
+    protsessor = 18
+    gpu = 1234
+    monitor  = 15
+
+t1 = Kompyuter()
+print(f"t1 malumotlar: ")
+print(f"obyekyning protsessor: {t1.protsessor}")
+print(f"obyekyning gpu: {t1.gpu}")
+print(f"obyekyning monitor : {t1.monitor }")
+
+
+
+
+# 11-m
+class Mashina:
+    gildirak = 4
+
+
+m1 = Mashina()
+print(f"m1.gildirak: {m1.gildirak}")
+
+m2 = Mashina()
+m2.rang = "oq"
+print(f"m2.rang: {m2.rang}")
+
+m3 = Mashina()
+m3.narx = 123456789
+print(f"m3.narx: {m3.narx}")
+
+m4 = Mashina()
+m4.madel = "Coblt"
+print(f"m4.madel: {m4.madel}")
+
+
+# 12-m
+class Telefon:
+    brand = "Samsung"
+
+
+t1 = Telefon()
+print(f"t1.brand: {t1.brand}")
+
+t2 = Telefon()
+t2.narx = 1234
+print(f"t2.narx: {t2.narx}")
+
+t3 = Telefon()
+t3.xotira = 128
+print(f"t3.xotira: {t3.xotira}")
+
+
+# 13-m
+class Kitob:
+    sahifa = 200
+
+k1 = Kitob()
+print(f"Kitob sahifa: {k1.sahifa}")
+
+k2 = Kitob()
+k2.nomi = "Harry"
+print(f"Kitob nomi: {k2.nomi}")
+
+
+k3 = Kitob()
+k3.muallif = "Mualif yo'q"
+print(f"Kitob mualif: {k3.muallif}")
+
+
+
+# 14-m
+class Talaba:
+    kurs = 1
+
+t1 = Talaba()
+print(f"kurs: {t1.kurs}")
+
+t2= Talaba()
+t2.ism = "Bunyod"
+print(f"ism: {t2.ism}")
+
+t3 = Talaba()
+t3.yosh = 15
+print(f"yosh: {t3.yosh}")
+
+
+# 15-m
+class Kompyuter:
+    ram = 8
+
+k1 = Kompyuter()
+print(f"ram : {k1.ram}")
+
+k2 = Kompyuter()
+k2.protsesor = 18
+print(f"protsesor : {k2.protsesor}")
+
+k3 = Kompyuter()
+k3.ssd = 2
+print(f"ssd : {k3.ssd}")
+
+
+
+# 16-m
+class Mashina:
+    rang = "OQ"
+    tezlik = 140
+    narx = 654321
+
+t1 = Mashina()
+print(f"t1 malumotlar: ")
+print(f"obyekyning rang: {t1.rang}")
+print(f"obyekyning tezlik: {t1.tezlik}")
+
+print(f"obyekyning narx: {t1.narx}")
+
+
+
+# 17-m
+class Telefon:
+    narx = 12345
+    kamera = 140
+    batareya = 100
+
+t1 = Telefon()
+print(f"t1 malumotlar: ")
+print(f"obyekyning narx: {t1.narx}")
+print(f"obyekyning kamera: {t1.kamera}")
+print(f"obyekyning batareya: {t1.batareya}")
+
+
+# 18-m
+class Kitob:
+    nomi = '......'
+    yili = 2000
+    janr  = 200
+
+t1 = Kitob()
+print(f"t1 malumotlar: ")
+print(f"obyekyning nomi: {t1.nomi}")
+print(f"obyekyning yili: {t1.yili}")
+print(f"obyekyning janr : {t1.janr }")
+
+
+# 19-m
+class Talaba:
+    ism = "Bunyod"
+    fakulted = "Alxorazimiy-vorislari"
+    baho = 2
+
+t1 = Talaba()
+print(f"t1 malumotlar: ")
+print(f"obyekyning ism: {t1.ism}")
+print(f"obyekyning fakulted: {t1.fakulted}")
+print(f"obyekyning baho : {t1.baho }")
+
+
+# 20-m
+class Kompyuter:
+    protsessor = 18
+    gpu = 1234
+    monitor  = 15
+
+t1 = Kompyuter()
+print(f"t1 malumotlar: ")
+print(f"obyekyning protsessor: {t1.protsessor}")
+print(f"obyekyning gpu: {t1.gpu}")
+print(f"obyekyning monitor : {t1.monitor }")
+
+
+
+
